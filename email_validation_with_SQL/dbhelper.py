f8c1a01e66ca2a39c7d1ec91a6e8a285217d9140
Mr. Shreyash <shreyashc2911@gmail.com>
2023-07-29 23:16:27 +0530

    updated dbhelper.py with genreation of outputfile

--- email_validation_with_SQL/dbhelper.py
+++ email_validation_with_SQL/dbhelper.py
@@ -4,23 +4,36 @@ import sys
 class DBhelper:
     def __init__(self):
         try:
-            self.conn = mysql.connector.connect(host= "localhost", user="root", password="", database = "flipkart_demo")
+            self.conn = mysql.connector.connect(host="localhost", user="root", password="", database="email")
             self.mycursor = self.conn.cursor()
-        except:
-            print("Some error occured. Could not connect to database")
+        except mysql.connector.Error as e:
+            print("Some error occurred. Could not connect to the database:", e)
             sys.exit(0)
         else:
-            print("Connected to database")
+            print("Connected to the database")
             
     def clean_table(self):
         try:
             query = """
-            SELECT * FROM user WHERE email LIKE 
+            SELECT * FROM user WHERE LOWER(EMAIL_ADDR) REGEXP '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$'
             """
 
             self.mycursor.execute(query)
-            self.conn.commit()
-        except:
+            data = self.mycursor.fetchall()
+
+            with open("output_file.sql", 'w') as f:
+                for row in data:
+                    values = ', '.join("'" + str(value) + "'" for value in row)
+                    sql_insert_statement = f"INSERT INTO user VALUES ({values});\n"
+                    f.write(sql_insert_statement)
+
+            # Close cursor and connection
+            self.mycursor.close()
+            self.conn.close()
+            
+        except mysql.connector.Error as e:
+            print("MySQL error:", e)
             return -1
         else:
+            print("Data retrieved successfully")
             return 1
